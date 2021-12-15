156c5b81d1e32f177822b3926404d9737584e56a
Nicolas <nico.r648@gmail.com>
2021-12-15 18:29:17 +0100

    Boid Movement

---
+++ Assets/Scripts/BoidAlignementBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Boid_m))]
+public class BoidAlignementBehaviour : MonoBehaviour
+{
+ 
+    private Boid_m boid;
+
+    public float radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boid = GetComponent<Boid_m>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var boids = FindObjectsOfType<Boid_m>();
+        var average = Vector3.zero;
+        var found = 0;
+
+        foreach(var boid in boids.Where(b => b != boid )) {
+            var diff = boid.transform.position - this.transform.position;
+            if (diff.magnitude < radius) {
+                average += boid.velocity;
+                found += 1;
+            }
+        }
+        if(found > 0) {
+            average = average / found;
+            boid.velocity += Vector3.Lerp(boid.velocity, average, Time.deltaTime);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/BoidInverseMagnetisimeBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Boid_m))]
+public class BoidInverseMagnetisimeBehaviour : MonoBehaviour
+{
+   private Boid_m boid;
+
+    public float radius;
+
+    public float repulsionForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boid = GetComponent<Boid_m>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var boids = FindObjectsOfType<Boid_m>();
+        var average = Vector3.zero;
+        var found = 0;
+
+        foreach(var boid in boids.Where(b => b != boid )) {
+            var diff = boid.transform.position - this.transform.position;
+            if (diff.magnitude < radius) {
+                average += diff;
+                found += 1;
+            }
+        }
+        if(found > 0) {
+            average = average / found;
+            boid.velocity -= Vector3.Lerp(Vector3.zero, average, average.magnitude / radius ) * repulsionForce;
+        }
+        
+    }
+}
